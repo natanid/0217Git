0c408770f4dc8c69c5b675d1ede29e3040f93ed8
natanid <natanid@yandex.ru>
2017-02-20 21:55:10 +0200

    0217

---
+++ 0217/src/ua/study/lesson05/Main.java
@@ -0,0 +1,74 @@
+package ua.study.lesson05;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// int[] numbers = { -121, -44, -55, -44, -778, -11545, -774, 0, 55555
+		// };
+		// int maxInt = numbers[0];
+		// for (int i = 0; i < numbers.length; i++) {
+		// if (numbers[i] > maxInt) {
+		// maxInt = numbers[i];
+		// }
+		// }
+		// System.out.println(maxInt);
+
+		// Scanner scanner = new Scanner(System.in);
+		// System.out.println("enter word");
+		// String text ="";
+		// String text1 ="";
+		// text = scanner.nextLine();
+		// for (int i = text.length()-1; i >= 0; i--) {
+		// text1 = text1 + text.charAt(i);
+		// }
+		// if(text.equals(text1)){
+		// System.out.println("палиндром");
+		// } else {
+		// System.out.println("not a палиндром");
+		// }
+
+		int _1 = 1;
+		int _2 = 2;
+		int _5 = 5;
+		int _10 = 10;
+		int _25 = 25;
+		int _50 = 50;
+		int number = 127;
+		int number1 = 0;
+		int number2 = 0;
+		int number3 = 0;
+		if (number > _50) {
+			number1 = number % _50;
+			System.out.println(number1);
+		}
+		if (number1 > _25) {
+			number2 = number % _25;
+			System.out.println(number2);
+		}
+		if (number1 > _25) {
+			number2 = number % _25;
+			System.out.println(number2);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
